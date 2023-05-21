d7e1c7107b0f654c244581a567b200da8dce74b4
oksagurzhyi <110240310+oksagurzhyi@users.noreply.github.com>
2023-05-21 20:07:00 +0000

    Deploying to gh-pages from @ oksagurzhyi/goit-js-hw-09@5f11a54f242232a94438ab7b06c04bb60bd503d5 🚀

---
+++ 01-color-switcher.fbdd6a01.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");a.disabled=!0,e.addEventListener("click",(function(){a.disabled=!1,d=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.disabled=!0})),a.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,a.disabled=!0}));var d=null}();
+//# sourceMappingURL=01-color-switcher.fbdd6a01.js.map
