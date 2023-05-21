5f11a54f242232a94438ab7b06c04bb60bd503d5
OksaGurzhyi <gurzhiy.oksa@gmail.com>
2023-05-21 22:06:31 +0200

    #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const body = document.querySelector('body');
 
+const btnStartElem = document.querySelector('[data-start]');
+
+const btnStopElem = document.querySelector('[data-stop]');
+
+btnStopElem.disabled = true;
+
+btnStartElem.addEventListener('click', startChangeColor);
+
+btnStopElem.addEventListener('click', stopChangeColor);
+
+let timerId = null;
+
+function startChangeColor() {
+  btnStopElem.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  btnStartElem.disabled = true;
+}
+
+function stopChangeColor() {
+  clearInterval(timerId);
+  btnStartElem.disabled = false;
+  btnStopElem.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
