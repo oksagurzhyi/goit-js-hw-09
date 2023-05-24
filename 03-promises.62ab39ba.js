c2c8f20717227bcd20289d03e98834e7a94c213e
oksagurzhyi <110240310+oksagurzhyi@users.noreply.github.com>
2023-05-24 10:30:04 +0000

    Deploying to gh-pages from @ oksagurzhyi/goit-js-hw-09@4db84892d386d1d7348c90786838344f59d86470 🚀

---
+++ 03-promises.62ab39ba.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".form");console.log(o);const e=document.querySelector("[button]");console.log(e),console.log(123);
+//# sourceMappingURL=03-promises.62ab39ba.js.map
---
+++ 03-promises.c1ddd613.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".form");console.log(o);var e=document.querySelector("[button]");console.log(e),console.log(123)}();
+//# sourceMappingURL=03-promises.c1ddd613.js.map
